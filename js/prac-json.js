a9ef7b053564808145eb327fccc3c8153d1deba9
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-08-22 19:45:44 +0300

    Committing

--- js/prac-json.js
+++ js/prac-json.js
@@ -1,18 +1,22 @@
 // Backend
 var place_post_on_page = function(data){
-  $('#display').append("<li>"+"<h2 class='clickable'>"+data.title+"</h2>"+"</li>")
+  $('#display').append("<li>"+"<button id='clickable'>"+data.title+"</button>"+"</li>")
 }
 
-var selected_post_on_page = function(data) {
-  $('#display').append("<li>"+"<h2 class='clickable'>"+
-  data.title+"</h2>"+"</li>"+"<h3>"+data.userId+"</h3>"+
-  "<h4>"+data.id+"</h4>"+"<p>"+data.body+"</p>")
-}
+// var selected_post_on_page = function(data) {
+//   $('#display').append("<li>"+"<h2 class='clickable'>"+
+//   data.title+"</h2>"+"</li>"+"<h3>"+data.userId+"</h3>"+
+//   "<h4>"+data.id+"</h4>"+"<p>"+data.body+"</p>")
+// }
 
 // Frontend
 $(document).ready(function(){
+  $("#clickable").click(function(){
+    console.log('asdf');
+        $(this).hide();
+    });
 
-  for (var index=1; index <= 10; index++) {
+  for (var index=1; index <= 4; index++) {
 
     $.get({
       url: 'http://jsonplaceholder.typicode.com/posts/'+index,
@@ -20,50 +24,57 @@ $(document).ready(function(){
         // console.log(data);
         place_post_on_page(data);
       }
-
     })
 
-    $('.clickable').click(function () {
-      // $('#display').text(" ")
-      $(this).selected_post_on_page(data);
-      // console.log("selected_post_on_page(data)");
-      // $('#display').text(" ");
-      // selected_post_on_page(data);
-    });
   }
-
-  // $.get({
-  //   url: 'http://jsonplaceholder.typicode.com/posts/2',
-  //   success: function(data) {
-  //     console.log(data);
-  //     place_post_on_page(data);
-  //   }
-  // })
 });
 
-  // $('#city1').click(function(event) {
-  //   event.preventDefault();
-  //   $('.clearField').empty();
-  //   alert("The data is currently being fetched");
-  //   $.get({
-  //     url: 'https://api.openweathermap.org/data/2.5/weather?q=Nairobi&mode=json&units=imperial&APPID=964b618a488e3dd53cdc4192294e9b98',
-  //     success: function(weather_data){
-  //       to_be_run_on_server_response(weather_data);
-  //     }
-  //   })
-  // });
 
-  // $('#city2').click(function(event) {
-  //   event.preventDefault();
-  //   $('.clearField').empty();
-  //   alert("The data is currently being fetched");
-  //   $.get({
-  //     url: 'https://api.openweathermap.org/data/2.5/weather?q=Mombasa&mode=json&units=imperial&APPID=964b618a488e3dd53cdc4192294e9b98',
-  //     success: function(weather_data){
-  //       to_be_run_on_server_response(weather_data);
-  //     }
-  //   })
-  // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#city1').click(function(event) {
+//   event.preventDefault();
+//   $('.clearField').empty();
+//   alert("The data is currently being fetched");
+//   $.get({
+//     url: 'https://api.openweathermap.org/data/2.5/weather?q=Nairobi&mode=json&units=imperial&APPID=964b618a488e3dd53cdc4192294e9b98',
+//     success: function(weather_data){
+//       to_be_run_on_server_response(weather_data);
+//     }
+//   })
+// });
+
+// $('#city2').click(function(event) {
+//   event.preventDefault();
+//   $('.clearField').empty();
+//   alert("The data is currently being fetched");
+//   $.get({
+//     url: 'https://api.openweathermap.org/data/2.5/weather?q=Mombasa&mode=json&units=imperial&APPID=964b618a488e3dd53cdc4192294e9b98',
+//     success: function(weather_data){
+//       to_be_run_on_server_response(weather_data);
+//     }
+//   })
+// });
 
 // var to_be_run_on_server_response = function(weather_data) {
 //   $('#temp').append(weather_data.main.temp);
@@ -77,3 +88,21 @@ $(document).ready(function(){
 //   $('#sunset').append(sunSet);
 
 // }
+
+// $.get({
+//   url: 'http://jsonplaceholder.typicode.com/posts/2',
+//   success: function(data) {
+//     console.log(data);
+//     place_post_on_page(data);
+//   }
+// })
+
+
+
+// $('.clickable').click(function () {
+//   // $('#display').text(" ")
+//   $(this).hide();
+//   // console.log("selected_post_on_page(data)");
+//   // $('#display').text(" ");
+//   // selected_post_on_page(data);
+// });
