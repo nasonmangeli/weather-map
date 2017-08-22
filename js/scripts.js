3f5ebf724809e210637c29b85a0c9bf3ca6d8fc5
nason mangeli <kuuchman@gmail.com>
2017-08-22 12:22:22 +0300

    Create functional links for different cities Create clickable links for Nairobi and Mombasa Change classes to id's Create clearField class Create click event listeners for city clickable links and clear the section tag when a city is clicked Centered text with css
    	modified:   css/styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,43 @@
 // })
 // Backend
 var to_be_run_on_server_response = function(weather_data) {
-  $('.temp').append(weather_data.main.temp);
-  $('.highTemp').append(weather_data.main.temp_max);
-  $('.lowTemp').append(weather_data.main.temp_min);
-  $('.description').append(weather_data.weather[0].description);
-  $('.windspeed').append(weather_data.wind.speed);
+  $('#temp').append(weather_data.main.temp);
+  $('#highTemp').append(weather_data.main.temp_max);
+  $('#lowTemp').append(weather_data.main.temp_min);
+  $('#description').append(weather_data.weather[0].description);
+  $('#windspeed').append(weather_data.wind.speed);
   var sunRise = new Date(weather_data.sys.sunrise * 1000);
-  $('.sunrise').append(sunRise);
+  $('#sunrise').append(sunRise);
   var sunSet = new Date(weather_data.sys.sunset * 1000);
-  $('.sunset').append(sunSet);
+  $('#sunset').append(sunSet);
+
 }
 
 // Frontend
-$.get({
-  url: 'http://api.openweathermap.org/data/2.5/weather?q=Nairobi&mode=json&units=imperial&APPID=964b618a488e3dd53cdc4192294e9b98',
-  success: function(weather_data){
-    to_be_run_on_server_response(weather_data);
-  }
-})
+$(document).ready(function(){
+
+  $('#city1').click(function(event) {
+    event.preventDefault();
+    $('.clearField').empty();
+    alert("The data is currently being fetched");
+    $.get({
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=Nairobi&mode=json&units=imperial&APPID=964b618a488e3dd53cdc4192294e9b98',
+      success: function(weather_data){
+        to_be_run_on_server_response(weather_data);
+      }
+    })
+  });
+
+  $('#city2').click(function(event) {
+    event.preventDefault();
+    $('.clearField').empty();
+    alert("The data is currently being fetched");
+    $.get({
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=Mombasa&mode=json&units=imperial&APPID=964b618a488e3dd53cdc4192294e9b98',
+      success: function(weather_data){
+        to_be_run_on_server_response(weather_data);
+      }
+    })
+  });
+
+});
